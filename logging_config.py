fe58d1a93a80fbae841f2ace559ff6699b6f4e0d
Mia Grigoreva <mia@nebius.com>
2025-07-24 16:52:52 +0000

    Set up logging config

---
+++ logging_config.py
@@ -0,0 +1,73 @@
+import logging
+import logging.config
+
+
+def setup_logging():
+    """Setup logging configuration using dictConfig"""
+
+    config = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "default": {
+                "format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+                "datefmt": "%Y-%m-%d %H:%M:%S",
+            },
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "level": "INFO",
+                "formatter": "default",
+                "stream": "ext://sys.stdout",
+            },
+        },
+        "loggers": {
+            "": {  # root logger
+                "level": "INFO",
+                "handlers": ["console"],
+            },
+            "app": {
+                "level": "INFO",
+                "handlers": ["console"],
+                "propagate": False,
+            },
+            "sqlalchemy.engine": {
+                "level": "WARN",
+                "handlers": ["console"],
+                "propagate": False,
+            },
+            "telethon_hook": {
+                "level": "INFO",
+                "handlers": ["console"],
+                "propagate": False,
+            },
+            "fetch_messages": {
+                "level": "INFO",
+                "handlers": ["console"],
+                "propagate": False,
+            },
+            "sync_dialogs": {
+                "level": "INFO",
+                "handlers": ["console"],
+                "propagate": False,
+            },
+            "dependency": {
+                "level": "INFO",
+                "handlers": ["console"],
+                "propagate": False,
+            },
+            "user_repository": {
+                "level": "INFO",
+                "handlers": ["console"],
+                "propagate": False,
+            },
+            "nebius": {
+                "level": "INFO",
+                "handlers": ["console"],
+                "propagate": False,
+            },
+        },
+    }
+
+    logging.config.dictConfig(config)
